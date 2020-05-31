ddc906a7a06099f94193e605b1c59384aa437e45
Harry Henry Gebel <hhgebel@gmail.com>
2020-05-30 20:08:58 -0400

    First commit, add function web-mode-element-unwrap

---
+++ lisp/web-editing.el
@@ -0,0 +1,10 @@
+;; Thanks to Andrew Stewart for providing this code on the Emacs wiki.
+(defun web-mode-element-unwrap ()
+  "Raise an element in the DOM, erasing its parent.
+Just like `paredit-splice-sexp+' style. Be careful to have the entire element selected
+or have the cursor on the opening bracket."
+  (interactive)
+  (save-excursion
+    (web-mode-element-parent)
+    (web-mode-element-vanish 1)
+    (back-to-indentation)))
