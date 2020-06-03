1baadb8ef44906170699ff8606892ae71b78ba69
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-03 04:57:13 -0400

    Untabify

--- lisp/window-manipulation.el
+++ lisp/window-manipulation.el
@@ -27,8 +27,8 @@ windows created, from left to right."
   (let ((frame (selected-frame)))
     (lambda (buffer _)
       (when (eq frame (selected-frame))
-        (set-window-buffer window buffer)
-        window))))
+	(set-window-buffer window buffer)
+	window))))
 
 (defun delete-stale-buffer-action-functions ()
   "Remove display buffer action functions that no longer point to live windows."
@@ -36,22 +36,22 @@ windows created, from left to right."
     (dolist (subitem item)
       ;; only check cons cell attached window property
       (when (consp subitem) 
-        (when (eq (car subitem) 'attached-window)
-          ;; Remove item when subitem no longer refers to a live window
-          ;; in addition to preventing display-buffer-alist from accumulating
-          ;; stale cruft, this should also free the window for garbage
-          ;; collection
-          (unless (window-live-p (cdr subitem))
-            (customize-set-variable
-             'display-buffer-alist
-             (remove item display-buffer-alist))))))))
+	(when (eq (car subitem) 'attached-window)
+	  ;; Remove item when subitem no longer refers to a live window
+	  ;; in addition to preventing display-buffer-alist from accumulating
+	  ;; stale cruft, this should also free the window for garbage
+	  ;; collection
+	  (unless (window-live-p (cdr subitem))
+	    (customize-set-variable
+	     'display-buffer-alist
+	     (remove item display-buffer-alist))))))))
 (add-hook
  'window-configuration-change-hook
  'delete-stale-buffer-action-functions)
 
 (let (original-window) ; holds selected window when function is first called
   (defun evenly-split-window (splits
-                              &optional orientation no-make-atom window-list)
+			      &optional orientation no-make-atom window-list)
     "Split a window into SPLITS equally sized windows.
 Splits the current windows into SPLITS evenly sized windows. Any
 left over characters are distributed to the left or top most
@@ -104,8 +104,8 @@ COUNT is the numeric prefix argument.  Returns nil."
   (interactive "p")
   ;; store information on original buffer status
   (let ((original-buffer (current-buffer))
-        (original-point (point))
-        new-window)
+	(original-point (point))
+	new-window)
     (bury-buffer) ; remove buffer from current window
     (other-window count) ; change selected window
     (switch-to-buffer original-buffer) ; bring buffer to new window
