e08e9bc722c3dbf031ab797da38613773f4a2b1a
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-20 16:33:44 -0400

    Fix incorrect name of magit in call to load

--- init.el
+++ init.el
@@ -130,7 +130,7 @@
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 (add-hook 'gitignore-mode-hook 'flyspell-prog-mode)
 ;; git
-(load "magit-mode") ; magit-after-save-refresh-status available at start
+(load "magit") ; magit-after-save-refresh-status available at start
 (global-unset-key (kbd "C-x g"))
 (global-set-key (kbd "<f5>") 'magit-status)
 (add-hook 'after-save-hook 'magit-after-save-refresh-status t)
