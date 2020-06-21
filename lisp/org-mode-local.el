02d3d74fa431969b1569f8c2e96e7f9368635b0b
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-21 16:20:04 -0400

    Preload org

--- lisp/org-mode-local.el
+++ lisp/org-mode-local.el
@@ -1,9 +1,10 @@
 ;;; -*- lexical-binding: t; -*-
 ;; functions to help with org-mode
 
+(load "org")
 (defun org-local-save ()
   "Keybinding to automatically perform exports on file save"
   (interactive)
   (save-buffer)
-  (org-latex-export-to-pdf))
-(define-key org-mode-map (kbd "<f10>") #'org-local-save)
+  (make-thread 'org-latex-export-to-pdf))
+(define-key org-mode-map (kbd "<f10>") 'org-local-save)
