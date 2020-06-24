ea2c0739707f176f2762a84562c344e962e41c3e
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-24 08:25:14 -0400

    Remove auto export on save

--- lisp/org-mode-local.el
+++ lisp/org-mode-local.el
@@ -2,9 +2,17 @@
 ;; functions to help with org-mode
 
 (load "org")
-(defun org-local-save ()
-  "Keybinding to automatically perform exports on file save"
-  (interactive)
-  (save-buffer)
-  (make-thread 'org-latex-export-to-pdf))
-(define-key org-mode-map (kbd "<f10>") 'org-local-save)
+
+;; (defun org-local-save ()
+;;   "Keybinding to automatically perform exports on file save"
+;;   (interactive)
+;;   (save-buffer)
+;;   (make-thread 'org-latex-export-to-pdf))
+;; (define-key org-mode-map (kbd "<f10>") 'org-local-save)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((ditaa . t))) ; this line activates ditaa
+
+;; TODO check ditaa location and set org-ditaa-jar-path instead of
+;; hard coding in customization system
